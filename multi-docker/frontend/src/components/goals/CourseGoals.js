b36266949e195f4959f727cde9a48964fea57fcb
Quoc Bui <thekingdom3036@gmail.com>
2023-01-08 02:02:56 +0700

    Update code

--- multi-docker/frontend/src/components/goals/CourseGoals.js
+++ multi-docker/frontend/src/components/goals/CourseGoals.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import './CourseGoals.css';
-import Card from '../UI/Card';
-import GoalItem from './GoalItem';
+import "./CourseGoals.css";
+import Card from "../UI/Card";
+import GoalItem from "./GoalItem";
 
 function CourseGoals(props) {
   const hasNoGoals = !props.goals || props.goals.length === 0;
 
   return (
-    <section id='course-goals'>
+    <section id="course-goals">
       <Card>
         {hasNoGoals && <h2>No goals found. Start adding some!</h2>}
         <ul>
